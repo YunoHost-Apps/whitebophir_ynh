2ce06a481249a9ed74b9eea81edbcb9f9952b8e1
oiseauroch <contact@oiseauroch.fr>
2022-10-14 11:00:41 +0200

    update to version v1.19.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-NODEJS_VERSION=12
+NODEJS_VERSION=16
 
 #=================================================
 # PERSONAL HELPERS
